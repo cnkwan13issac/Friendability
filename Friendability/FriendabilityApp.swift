b74d50a04507a7c1121742874f062f3bc6fb418e
Issac Kwan <cnkwan13issac@gmail.com>
2023-11-23 12:18:51 +0000

    Initial Commit

---
+++ Friendability/FriendabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendabilityApp.swift
+//  Friendability
+//
+//  Created by Kwan Ching Nam on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
